22c22a8225be1c22d1092f845bb6d1fe2e7032b0
Simon <simon.hessel@kreios.lu>
2021-10-13 19:04:40 +0200

    Switched to esbuild

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,16 +1,35 @@
-import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json";
+import dts from "rollup-plugin-dts";
+import esbuild from "rollup-plugin-esbuild";
+import { defineConfig } from "rollup";
 
-export default [
-  {
-    input: "lib/index.ts",
-    external: ["ms"],
-    plugins: [
-      typescript(), // so Rollup can convert TypeScript to JavaScript
-    ],
+const bundle = (config) => ({
+  ...config,
+  input: "lib/index.ts",
+  external: (id) => !/^[./]/.test(id),
+});
+
+export default defineConfig([
+  bundle({
+    plugins: [esbuild()],
     output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: "es",
+        sourcemap: true,
+      },
     ],
-  },
-];
+  }),
+  bundle({
+    plugins: [dts()],
+    output: {
+      file: pkg.typings,
+      format: "es",
+    },
+  }),
+]);
